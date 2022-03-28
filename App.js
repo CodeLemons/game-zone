473d82543414b801344a34cde44f7c2b1e7919c0
melonchmp <asamalihamza@gmail.com>
2022-03-28 12:18:35 +0100

    Added a function that loads external fonts asyncro

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import Home from './screens/home';
+import * as Font from 'expo-font';
+import { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+});
 
 export default function App() {
-  return (
-    <Home />
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded){
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading 
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={() => console.log('error')}
+      />
+    )
+  }
 }
 
