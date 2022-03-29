dae665c851de83514381e7b0cd0986eb32090d38
melonchmp <asamalihamza@gmail.com>
2022-03-29 13:59:46 +0100

    Added updated react-navigation stack, and routes between screens soon to be implemented

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import ReviewDetails from './screens/reviewDetails';
+import AppLoading from 'expo-app-loading';
 import Home from './screens/home';
 import * as Font from 'expo-font';
 import { useState } from 'react';
-import AppLoading from 'expo-app-loading';
 
 const getFonts = () => Font.loadAsync({
     'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
     'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
 });
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   if(fontsLoaded){
     return (
-      <Home />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home" 
+            component={Home} 
+            options={{ title: 'GameZone' }}
+          />
+          <Stack.Screen
+            name="ReviewDetails" 
+            component={ReviewDetails}
+            options={{ title: 'Review Details' }} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   } else {
     return (
