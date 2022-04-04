6f5e07f0fc9e9c25dc51885c8ed1a2a8fc4cdfe4
melonchmp <asamalihamza@gmail.com>
2022-04-04 14:25:28 +0100

    Added form validation using the YUP package. Also styled the error texts.

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -1,12 +1,28 @@
-import { StyleSheet, Button, TextInput, View, Text } from  'react-native';
+import { Button, TextInput, View, Text } from  'react-native';
 import { globalStyles } from '../styles/global';
 import { Formik } from 'formik';
+import * as yup from 'yup';
+
+const reviewSchema = yup.object({
+    title: yup.string()
+              .required()
+              .min(4),
+    body: yup.string()
+             .required()
+             .min(8),
+    rating: yup.string()
+               .required()
+               .test('is-num-1-5', 'Rating must be a number from 1 - 5', (val) =>{
+                   return parseInt(val) < 6 && parseInt(val) > 0
+               })     
+})
 
 export default function ReviewForm({ addReview }) {
     return (
         <View style={globalStyles.container}>
             <Formik
                 initialValues={{ title: '', body: '', rating: '' }}
+                validationSchema={reviewSchema}
                 onSubmit={(values) => {
                     addReview(values);
                 }}
@@ -18,7 +34,9 @@ export default function ReviewForm({ addReview }) {
                             placeholder='Review title'
                             onChangeText={formikProps.handleChange('title')}
                             value={formikProps.values.title}
+                            onBlur={formikProps.handleBlur('title')}
                         />
+                        <Text style={globalStyles.errorText}>{ formikProps.touched.title && formikProps.errors.title }</Text>
 
                         <TextInput
                             multiline 
@@ -26,7 +44,9 @@ export default function ReviewForm({ addReview }) {
                             placeholder='Review body'
                             onChangeText={formikProps.handleChange('body')}
                             value={formikProps.values.body}
+                            onBlur={formikProps.handleBlur('body')}
                         />
+                        <Text style={globalStyles.errorText}>{ formikProps.touched.body && formikProps.errors.body }</Text>
 
                         <TextInput 
                             style={globalStyles.input}
@@ -34,7 +54,9 @@ export default function ReviewForm({ addReview }) {
                             onChangeText={formikProps.handleChange('rating')}
                             value={formikProps.values.rating}
                             keyboardType='numeric'
+                            onBlur={formikProps.handleBlur('rating')}
                         />
+                        <Text style={globalStyles.errorText}>{ formikProps.touched.rating && formikProps.errors.rating }</Text>
 
                         <Button title='submit' color='maroon' onPress={formikProps.handleSubmit} />
                     </View>
